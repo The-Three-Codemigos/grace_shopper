aeb3313e73a116b1349621351ead8a122fefe76f
Matthew Short <short.matthew.f@gmail.com>
2020-05-21 15:32:46 -0400

    better log messages

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ const { client } = require('./db');
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, async () => {
-  console.log(`Server is running on ${ PORT }`);
+  console.log(`Server is running on ${ PORT }!`);
 
   try {
-    await client.connect()
+    await client.connect();
+    console.log('Database is open for business!');
   } catch (error) {
-    console.error("Client is unable to connect\n", error);
+    console.error("Database is closed for repairs!\n", error);
   }
 });
