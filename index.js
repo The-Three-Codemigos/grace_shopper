2be9ec6166efca1bbbb6caaa508e005d79916133
shapirodaniel <shapirodanieladam@gmail.com>
2022-01-13 23:37:55 -0600

    add: github actions process.env.CI check to start client connection

--- index.js
+++ index.js
@@ -31,11 +31,14 @@ const PORT = process.env.PORT || 4000;
 const handle = server.listen(PORT, async () => {
   console.log(`Server is running on ${PORT}!`);
 
-  try {
-    await client.connect();
-    console.log('Database is open for business!');
-  } catch (error) {
-    console.error('Database is closed for repairs!\n', error);
+  // if server is started in github actions context skip db connection
+  if (!process.env.CI) {
+    try {
+      await client.connect();
+      console.log('Database is open for business!');
+    } catch (error) {
+      console.error('Database is closed for repairs!\n', error);
+    }
   }
 });
 
