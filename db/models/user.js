3207729f7ac1a1e026af63d982e41cc0985e0f27
shapirodaniel <shapirodanieladam@gmail.com>
2022-01-29 15:18:56 -0600

    add: client access user model

--- db/models/user.js
+++ db/models/user.js
@@ -1,5 +1,5 @@
 // grab our db client connection to use with our adapters
-const { client } = require('../');
+const client = require('../client');
 
 module.exports = {
   // add your database adapter fns here
