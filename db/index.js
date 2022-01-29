a9093453a341f10e8c8dcf0597742d8029f91246
shapirodaniel <shapirodanieladam@gmail.com>
2022-01-29 15:20:31 -0600

    update: db/index.js

--- db/index.js
+++ db/index.js
@@ -1,14 +1,4 @@
-// Connect to DB
-const { Client } = require('pg');
-
-// change the DB_NAME string to whatever your group decides on
-const DB_NAME = 'change-this-name';
-
-const DB_URL =
-  process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
-
-const client = new Client(DB_URL);
-
+const client = require('./client');
 const models = require('./models');
 
 module.exports = {
