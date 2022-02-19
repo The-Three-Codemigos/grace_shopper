018058fc3456d110b72bf7599a429d369618f10a
shapirodaniel <shapirodanieladam@gmail.com>
2022-02-19 17:23:48 -0600

    update: cicd yaml for postgres container to support api tests

--- db/client.js
+++ db/client.js
@@ -7,6 +7,22 @@ const DB_NAME = 'univ-boilerplate-test';
 const DB_URL =
   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
 
-const client = new Client(DB_URL);
+let client;
+
+// important! the client instance must be initialized with user/password
+// when running in the GitHub Actions CI environment
+if (process.env.CI) {
+  client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+  });
+  // otherwise, client is instantiated with the Heroku database url
+  // or the local devenv connection string
+} else {
+  client = new Client(DB_URL);
+}
 
 module.exports = client;
