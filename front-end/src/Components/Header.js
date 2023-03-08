a34382bb4428afc646d1d09cae3ae332d4fd00c4
UlyssesCortes <ulyssescortes13@gmail.com>
2023-03-07 19:52:31 -0600

    search bar input added

--- front-end/src/Components/Header.js
+++ front-end/src/Components/Header.js
@@ -14,16 +14,21 @@ const Header = ({ isLoggedIn, setIsLoggedIn }) => {
                     <li><Link to='/' className="headerLink">HOME</Link></li>
                     <li><Link to='/about' className="headerLink">ABOUT</Link></li>
                     <li><Link to='/products' className="headerLink">PRODUCTS</Link></li>
-
                 </ul>
                 <ul className="list">
-                    <li><img className="headerIcon" src="https://cdn-icons-png.flaticon.com/512/54/54481.png" alt="searchIcon" /></li>
+                    <li>
+                        <div class="box">
+                            <form name="search">
+                                <input type="text" class="input" name="txt" onmouseout="this.value = ''; this.blur();" placeholder="Search...">
+                                </input>
+                                <img className="headerIcon" src="https://cdn-icons-png.flaticon.com/512/54/54481.png" alt="searchIcon" />
+                            </form>
+                        </div>
+                    </li>
                     <li><Link to='/cart' className="headerIcon"><img className="headerIcon" src="https://cdn-icons-png.flaticon.com/512/2832/2832495.png" alt="searchIcon" /></Link></li>
-                    {/* {isLoggedIn ? */}
                     <li><Link to='/profile' className="headerIcon"><img className="headerIcon" src="https://cdn-icons-png.flaticon.com/512/1077/1077063.png" alt="searchIcon" /></Link></li>
-                    {/* <li><button className="loginButtonHeader">SignIn</button></li> */}
-                    {/* } */}
                 </ul>
+
             </nav>
         </>
     )
