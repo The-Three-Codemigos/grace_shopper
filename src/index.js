03b4cf5a93b8570bb6ebac6928a22f2556da92d7
UlyssesCortes <ulyssescortes13@gmail.com>
2023-03-08 19:36:04 -0600

    backend fixed

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Components/Home';
 
 const App = () => {
     return (
         <>
-            {/* <Home /> */}
-            <BrowserRouter>
-                <Routes>
-                    <Route
-                        path='/'
-                        element={<Home />}
-                    />
-                </Routes>
-            </BrowserRouter>
+            <h1>Home</h1>
         </>
     )
 }
